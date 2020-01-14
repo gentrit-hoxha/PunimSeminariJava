06d83307d223e816c2fa8ecf507366e532c21b69
Gentrit Hoxha <56977811+gentrit-hoxha@users.noreply.github.com>
2020-01-14 01:00:15 +0100

    Update Input_Rroga.java

--- PunimSeminariJAVA-GentritHoxha/src/Input_Rroga.java
+++ PunimSeminariJAVA-GentritHoxha/src/Input_Rroga.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
-/** Input_Rroga eshte klase view e cila inicializon variblat duke shfrytezuar klasen Model_Rroga */
+/** Input_Rroga eshte klase view e cila inicializon variblat
+* duke shfrytezuar klasen Model_Rroga */
 public class Input_Rroga{
 
   //Eshte krijuar nje objekt i klases modeluese per ti inicializuar variablat  
@@ -8,25 +9,28 @@ public class Input_Rroga{
 
   /** Konstruktor qe permban instrusionet per ti inicializuar variablat nga klasa modeluese*/
    public  Input_Rroga(){
-    
+     
       model.emri = JOptionPane.showInputDialog("Shenoni emrin tuaj: ");
       model.mbiemri = JOptionPane.showInputDialog("Shenoni mbiemrin tuaj: ");
       model.pozita = JOptionPane.showInputDialog("Shenoni poziten tuaj te punes: "); 
-     
-       model.kontrolla_inputit();
+      model.kontrolla_inputit();
+      
       //Eshte perdoruar try dhe catch qe vlerat si input te jene vetem numra 
-      //ndersa nese shtyp ndonje karakter tjeter atehere te kthen nje messageDialog "shenoni nje numer"
+      //ndersa nese shtyp ndonje karakter tjete r atehere te kthen nje messageDialog "shenoni nje numer"
       //Dhe prape shfaqe dritaren per te shkruar nje numer 
+   
+     
       try{
          String paga_per_ore_rregullt = JOptionPane.showInputDialog("Sa eshte paga per 1 ore pune te rregullt: ");
-         model.paga_rregullt = new Double( paga_per_ore_rregullt).doubleValue();
+         model.paga_rregullt = new Double( paga_per_ore_rregullt).doubleValue();   
       }
+      
       catch(Exception e){
          JOptionPane.showMessageDialog(null,"GABIM: Ju lutem shenoni nje numer ");
          String paga_per_ore_rregullt = JOptionPane.showInputDialog("Sa eshte paga per 1 ore pune te rregullt: ");
-         model.paga_rregullt   = new Double( paga_per_ore_rregullt).doubleValue();
+         model.paga_rregullt   = new Double(paga_per_ore_rregullt).doubleValue();
       }
-     
+      
       try{
          String  ore_pune = JOptionPane.showInputDialog("Sa ore keni punuar gjate kesaj jave:  ");
          model.ora_rregullt   = new Integer(ore_pune).intValue();
@@ -37,5 +41,7 @@ public class Input_Rroga{
          model.ora_rregullt   = new Integer(ore_pune).intValue();
       }
    
+          
+          
    }
 }
